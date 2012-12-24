5c25afae2fde74d854f32196774407e9d3f7702f
Nathan S <nathan@inspire9.com>
2012-12-24 12:30:40 +1100

    whitespace

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ActiveRecord::Base
 
   attr_accessible :title, :body, :last_activity_at
-  
+
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :subscriptions, as: :subscribable, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
   belongs_to :user
-  
+
   after_create do
     s = subscriptions.new last_read_at: Time.now
     s.user_id = user_id
